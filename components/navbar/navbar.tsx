3f9d0ee4d2106dcb5d94b5b03a8e7cc4e1ce0a53
TreNgheDiCode <gabayan170@gmail.com>
2024-01-29 15:08:30 +0700

    Fix apperance protected navbar and add light mode

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import {
+  Avatar,
   Divider,
-  Image,
   Input,
   Link,
   Navbar,
@@ -10,7 +10,6 @@ import {
   NavbarContent,
   NavbarItem,
   NavbarMenu,
-  NavbarMenuItem,
   NavbarMenuToggle,
 } from "@nextui-org/react";
 import { UserButton } from "./user-button";
@@ -21,6 +20,7 @@ import { usePathname } from "next/navigation";
 import { ManagementDropdown } from "./management-dropdown";
 import { MobileManagementDropdown } from "./mobile-management-dropdown";
 import { DictionaryLanguage } from "@/data/dictionaries";
+import Image from "next/image";
 
 interface ProtectedNavbarProps {
   user: ExtendedUser;
@@ -36,9 +36,11 @@ export const ProtectedNavbar = ({ user, dict }: ProtectedNavbarProps) => {
     <Navbar
       isBordered
       onMenuOpenChange={setIsOpen}
+      className="bg-white dark:bg-background"
       classNames={{
         wrapper: "max-w-full flex h-[85px] p-0 pl-3 pr-6",
-        menu: ["top-[85px]"],
+        toggleIcon: "text-[#7D1f1F] dark:text-primary",
+        menu: ["top-[85px] bg-white dark:bg-background"],
         item: [
           "flex",
           "relative",
@@ -51,7 +53,7 @@ export const ProtectedNavbar = ({ user, dict }: ProtectedNavbarProps) => {
           "data-[active=true]:after:right-0",
           "data-[active=true]:after:h-[2px]",
           "data-[active=true]:after:rounded-[2px]",
-          "data-[active=true]:after:bg-primary data-[active=true]:dark:after:bg-[#7D1F1F]",
+          "data-[active=true]:after:bg-[#7D1F1F] data-[active=true]:dark:after:bg-primary",
         ],
       }}
     >
@@ -62,20 +64,39 @@ export const ProtectedNavbar = ({ user, dict }: ProtectedNavbarProps) => {
           className="md:hidden"
         />
         <NavbarBrand>
-          <Image alt="logo" src="/logo-red.png" className="h-fit" />
-          <p className="font-bold text-inherit">Quan Ly Du Hoc</p>
+          <Image
+            height={85}
+            width={92}
+            priority={true}
+            style={{ width: "auto", height: "auto" }}
+            quality={100}
+            alt="logo"
+            src="/logo-red.webp"
+            className="h-fit"
+          />
+          <p className="font-bold text-[#7D1F1F] dark:text-primary">
+            Quan Ly Du Hoc
+          </p>
         </NavbarBrand>
       </NavbarContent>
       {/* Navigation */}
       <NavbarContent className="hidden gap-4 md:flex" justify="start">
         <NavbarItem isActive={pathname.includes("/profile")}>
-          <Link color="primary" href="/student/profile">
+          <Link
+            color="primary"
+            href="/student/profile"
+            className="text-[#7D1F1F] dark:text-primary"
+          >
             Profile
           </Link>
         </NavbarItem>
         <ManagementDropdown />
         <NavbarItem isActive={pathname.includes("/settings")}>
-          <Link color="primary" href="/student/settings">
+          <Link
+            color="primary"
+            href="/student/settings"
+            className="text-[#7D1F1F] dark:text-primary"
+          >
             Settings
           </Link>
         </NavbarItem>
@@ -98,9 +119,18 @@ export const ProtectedNavbar = ({ user, dict }: ProtectedNavbarProps) => {
       </NavbarContent>
       {/* Mobile Menu */}
       <NavbarMenu>
-        <div className="py-1.5">
-          <p className="font-semibold">{user.name}</p>
-          <p className="font-semibold">{user.studentCode}</p>
+        <div className="flex items-center gap-x-4 py-1.5">
+          <Avatar
+            isBordered
+            color="secondary"
+            name={"Image"}
+            size="sm"
+            src={user.image || "/placeholder.webp"}
+          />
+          <div>
+            <p className="font-semibold text-primary">{user.name}</p>
+            <p className="font-semibold text-primary">{user.studentCode}</p>
+          </div>
         </div>
         <Divider />
         <MobileManagementDropdown />
